d7f2c070fa35e746014a51ed6a4345da9db44ed4
Leonardo2106 <leonardo.piassa07@gmail.com>
2025-05-23 20:24:26 -0300

    Adicionando primeiros arquivos

---
+++ cadastro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+struct dispositivo {
+	char modelo[25];
+	char marca[25];
+	int ano;
+};
+
+typedef struct dispositivo inf_dispositivo;
+
+int main() {
+	printf("Olá arthur");
+}
